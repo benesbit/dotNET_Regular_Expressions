00e34d69fdaaf2a499f643641c4f8f0f23f9d211
Ben Nesbit <benesbit@yahoo.com>
2020-09-30 14:20:00 -0700

    Add regex example for parsing phone numbers.

--- RegexDemonstration/Program.cs
+++ RegexDemonstration/Program.cs
@@ -12,9 +12,58 @@ namespace RegexDemonstration
             escapeCharacterRegexExample();
         }
 
+        public static void phoneNumbersRegex()
+        {
+            var patterns = new List<string>
+            {
+                @"\d\d\d-\d\d\d-\d\d\d\d"
+            };
+
+            var inputs = new List<string>
+            {
+                "5555555555",
+                "(555)-555-5555",
+                "012-345-6789",
+                "555-555-5555",
+                "555-555-555a",
+                "5555-555-5555",
+                "555-5555555",
+                "000-000-0000",
+                "a",
+                "5.55-555-5555",
+                "...-...-....",
+            };
+
+            patterns.ForEach(pattern =>
+            {
+                Console.WriteLine($"Regular expression: {pattern}");
+                var regex = new Regex(pattern);
+
+                inputs.ForEach(input =>
+                {
+                    Console.WriteLine($"\tInput pattern: {input}");
+
+                    var results = regex.Matches(input);
+
+                    if (results.Count <= 0)
+                    {
+                        Console.WriteLine("\t\tNo matches found.");
+                    }
+                    else
+                    {
+                        foreach (Match result in results)
+                        {
+                            Console.WriteLine($"\t\tMatch found at index {result.Index}. Length: {result.Length}.");
+                        }
+                    }
+                });
+            });
+            Console.ReadKey();
+        }
+
         public static void escapeCharacterRegexExample()
         {
-            var patterns = new List<string> { "\\?", @"\?", Regex.Escape("?") };
+            var patterns = new List<string> { "\\", @"\?", Regex.Escape("?") };
             var inputs = new List<string> { "?" };
 
             patterns.ForEach(pattern =>
