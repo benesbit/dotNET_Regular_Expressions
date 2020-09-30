c9b088645293d5bb3378677199faf852376cfe62
Ben Nesbit <benesbit@yahoo.com>
2020-09-30 16:11:27 -0700

    Add call to 'regexExpressionOptions' method.

--- RegexDemonstration/Program.cs
+++ RegexDemonstration/Program.cs
@@ -14,6 +14,7 @@ namespace RegexDemonstration
             //phoneNumbersRegex();
             //parseReceipts();
             //regexAnchorsAndBoundaries();
+            regexExpressionOptions();
         }
 
         public static void regexExpressionOptions()
