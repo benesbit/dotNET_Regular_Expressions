d01d24d323b984d661228225f2e8010c1ba512cf
Ben Nesbit <benesbit@yahoo.com>
2020-10-01 10:48:26 -0700

    Create a stopwatch for timing the regex runaways.

--- RegexDemonstration/Program.cs
+++ RegexDemonstration/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -42,6 +43,8 @@ namespace RegexDemonstration
                 inputs.ForEach(input =>
                 {
                     Console.WriteLine($"\tInput pattern: {input}");
+                    var watch = new Stopwatch();
+                    watch.Start();
                 });
             });
         }
