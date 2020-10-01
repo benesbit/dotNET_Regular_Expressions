69fbc687547430a57e5990311fd3a6be5b577b09
Ben Nesbit <benesbit@yahoo.com>
2020-10-01 10:15:19 -0700

    Display results to screen, or message if no results.

--- RegexDemonstration/Program.cs
+++ RegexDemonstration/Program.cs
@@ -50,7 +50,20 @@ namespace RegexDemonstration
 
                 var regex = new Regex(pattern);
 
-                
+                inputs.ForEach(input =>
+                {
+                    Console.WriteLine($"\tInput pattern: {input}");
+
+                    var results = regex.Matches(input);
+                    if (results.Count <= 0)
+                    {
+                        Console.WriteLine($"\t\tNo matches found.");
+                    }
+                    foreach (Match result in results)
+                    {
+                        Console.WriteLine($"\t\tMatch found at index {result.Index}. Length: {result.Length}");
+                    }
+                });
             });
         }
 
